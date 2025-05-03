3850384ac2ca2bbe0fc7a4debb116fe54a65b011
Linhao Yuan <yuanlinhao1999@gmail.com>
2025-05-03 04:10:14 -0400

    Database

--- backend/app/database.py
+++ backend/app/database.py
@@ -3,19 +3,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-
-from dotenv import load_dotenv
-
-load_dotenv()
-
 DATABASE_URL = os.getenv("DATABASE_URL")
-
-if DATABASE_URL is None:
-    raise ValueError("DATABASE_URL is not set. Did you forget to define it?")
 
 if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
