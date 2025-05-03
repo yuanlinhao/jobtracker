9aaa6845b61c1a33089adbb070469c5c0294b89f
Linhao Yuan <yuanlinhao1999@gmail.com>
2025-05-03 03:56:35 -0400

    Database

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -5,6 +5,7 @@ from pydantic_settings import BaseSettings
 load_dotenv()
 
 class Settings(BaseSettings):
+    DATABASE_URL: str
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
