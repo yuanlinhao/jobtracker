49d95a7d1de49c39ecafea1f3af0e0268e67ba2c
Linhao Yuan <yuanlinhao1999@gmail.com>
2025-05-03 04:06:56 -0400

    Database

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,10 +1,21 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = "postgresql://localhost/jobtracker_dev"
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL is None:
+    raise ValueError("DATABASE_URL is not set. Did you forget to define it?")
+
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(DATABASE_URL)
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
